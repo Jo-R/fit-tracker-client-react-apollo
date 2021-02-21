8512435b4e425e9678fb6b3b95640423e1a9f257
Jo-R <jo.rigg8@gmail.com>
2021-02-21 21:28:41 +0000

    add you need to log in - checking reactive var updates other components

--- src/layout/HomePage.jsx
+++ src/layout/HomePage.jsx
@@ -2,12 +2,20 @@ import React from "react";
 import { PageLayout } from "./PageLayout";
 import { AddRunActivity } from "../features/runActivities/AddRunActivity";
 import { RunActivities } from "../features/runActivities/RunActivities";
+import { useIsLoggedIn } from "../apollo/reactiveVars";
 
 export const HomePage = ({ children, props }) => {
+  const { isLoggedIn } = useIsLoggedIn();
   return (
     <PageLayout>
-      <AddRunActivity {...props} />
-      <RunActivities {...props} />
+      {isLoggedIn ? (
+        <>
+          <AddRunActivity {...props} />
+          <RunActivities {...props} />
+        </>
+      ) : (
+        <p>you need to log in...</p>
+      )}
     </PageLayout>
   );
 };
