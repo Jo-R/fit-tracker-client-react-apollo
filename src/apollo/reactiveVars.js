7192cf37d83787c6c88861ce9521ed6672da9083
Jo-R <jo.rigg8@gmail.com>
2021-05-31 16:46:58 +0100

    comment

--- src/apollo/reactiveVars.js
+++ src/apollo/reactiveVars.js
@@ -2,6 +2,7 @@ import { isLoggedInVar } from "./client";
 import { useQuery } from "@apollo/client";
 import { USER_LOGIN_STATUS } from "./queries";
 
+// TODO why on earth did I make this a hook? should be context
 export const useIsLoggedIn = () => {
   const { data } = useQuery(USER_LOGIN_STATUS);
 
